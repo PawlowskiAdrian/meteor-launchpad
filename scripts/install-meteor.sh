bd5542e4016c9327d6478e64ea4ec2dec9257fee
PawlowskiAdrian <petroniusz2008@gmail.com>
2021-05-06 17:32:16 +0200

    tmp dir

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -e
-CURRENT_DIR=$(pwd)
+TMP_DIR=/tmp
+# $(pwd)
 
 if [ "$DEV_BUILD" = true ]; then
   # if this is a devbuild, we don't have an app to check the .meteor/release file yet,
@@ -10,19 +11,19 @@ if [ "$DEV_BUILD" = true ]; then
   curl -v https://install.meteor.com/ | sh
 else
   # download installer script
-  curl -v https://install.meteor.com -o $CURRENT_DIR/install_meteor.sh
+  curl -v https://install.meteor.com -o $TMP_DIR/install_meteor.sh
 
   # read in the release version in the app
   METEOR_VERSION=$(head $APP_SOURCE_DIR/.meteor/release | cut -d "@" -f 2)
 
   # set the release version in the install script
-  sed -i.bak "s/RELEASE=.*/RELEASE=\"$METEOR_VERSION\"/g" $CURRENT_DIR/install_meteor.sh
+  sed -i.bak "s/RELEASE=.*/RELEASE=\"$METEOR_VERSION\"/g" $TMP_DIR/install_meteor.sh
 
   # replace tar command with bsdtar in the install script (bsdtar -xf "$TARBALL_FILE" -C "$INSTALL_TMPDIR")
   # https://github.com/jshimko/meteor-launchpad/issues/39
-  sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" $CURRENT_DIR/install_meteor.sh
+  sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" $TMP_DIR/install_meteor.sh
 
   # install
   printf "\n[-] Installing Meteor $METEOR_VERSION...\n\n"
-  sh $CURRENT_DIR/install_meteor.sh
+  sh $TMP_DIR/install_meteor.sh
 fi
