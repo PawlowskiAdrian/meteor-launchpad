751927475c9a88b3a5b68bb53bd0efd0f05fa0fa
PawlowskiAdrian <petroniusz2008@gmail.com>
2021-05-10 13:16:17 +0200

    modified logging messages, added export for custom user

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -2,6 +2,8 @@
 if [ -f $APP_SOURCE_DIR/launchpad.conf ]; then
   source <(grep USERNAME_CUSTOM_PASS $APP_SOURCE_DIR/launchpad.conf)
   source <(grep USERNAME_CUSTOM_NAME $APP_SOURCE_DIR/launchpad.conf)
+  export $USERNAME_CUSTOM_NAME
+  export $USERNAME_CUSTOM_PASS
 fi
 
 if [ $USERNAME_CUSTOM_NAME ]; then
@@ -9,21 +11,21 @@ if [ $USERNAME_CUSTOM_NAME ]; then
     if [ $(id -u) -eq 0 ]; then
         egrep "^$USERNAME_CUSTOM_NAME" /etc/passwd >/dev/null
         if [ $? -eq 0 ]; then
-            echo "$USERNAME_CUSTOM_NAME exists!"
+            printf "$USERNAME_CUSTOM_NAME exists!"
             exit 1
         else
             if ! [ $USERNAME_CUSTOM_PASS ]; then
-                echo "USERNAME_CUSTOM_PASS not specified, using: password"
+                printf "USERNAME_CUSTOM_PASS not specified, using: password"
                 USERNAME_CUSTOM_PASS=$(echo "password")
             fi
             pass=$(perl -e 'print crypt($ARGV[0],"passwordSecret")' $USERNAME_CUSTOM_PASS)
             useradd -m -p "$pass" "$USERNAME_CUSTOM_NAME"
-            if [ $? -eq 0 ] && echo "User: $USERNAME_CUSTOM_NAME, has been added to system." || echo "Failed to add a user!"
+            if [ $? -eq 0 ] && printf "User: $USERNAME_CUSTOM_NAME, has been added to system." || printf "Failed to add a user!"
             usermod -aG sudo "$USERNAME_CUSTOM_NAME"
-            if [ $? -eq 0 ] && echo "User: $USERNAME_CUSTOM_NAME, has been added to sudo group." || echo "Failed to add user to sudoers!"
+            if [ $? -eq 0 ] && printf "User: $USERNAME_CUSTOM_NAME, has been added to sudo group." || printf "Failed to add user to sudoers!"
         fi
     else
-        echo "Only root may add a user to the system!"
+        printf "Only root may add a user to the system!"
         exit 2
     fi
 fi
--- scripts/switch-user.sh
+++ scripts/switch-user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $USERNAME_CUSTOM_NAME ]; then
     su $USERNAME_CUSTOM_NAME
-    echo "Proceeding as: $(whoami)."
+    printf "Proceeding as: $(whoami)."
     cd
     echo $USERNAME_CUSTOM_PASS | sudo -S chmod -R 750 $BUILD_SCRIPTS_DIR
 else
-    echo "USERNAME_CUSTOM_NAME has not been specified! Proceeding as: $(whoami)."
+    printf "USERNAME_CUSTOM_NAME has not been specified! Proceeding as: $(whoami)."
     exit 1
 fi
