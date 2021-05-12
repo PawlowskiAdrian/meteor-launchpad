a87c88f6d5917947d6b87bd819f5b9f9cc32a9a1
PawlowskiAdrian <petroniusz2008@gmail.com>
2021-05-12 14:13:19 +0200

    .

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -13,8 +13,8 @@ if [ "$DEV_BUILD" = true ]; then
   if [ "$METEOR_VERSION_CUSTOM" ]; then
     printf "\n[-] Installing Meteor %s...\n\n" "$METEOR_VERSION_CUSTOM"
     curl -v https://install.meteor.com/ | sh 
-    ls -laht ~/.meteor/
-    whoami
+    echo $(ls -laht ~/.meteor/)
+    echo $(whoami)
     meteor update --release "$METEOR_VERSION_CUSTOM"
   else
     printf "\n[-] Installing the latest version of Meteor...\n\n"
