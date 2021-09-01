75c8632c946dcb8b027dfc964cd2f931ea8fae02
PawlowskiAdrian <petroniusz2008@gmail.com>
2021-09-01 13:02:54 +0200

    remove rm GNUPGHOME: CircleCI received exit code 1

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -37,7 +37,8 @@ export GNUPGHOME="$(mktemp -d)"
 gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
 gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu
 
-rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc
+# "$GNUPGHOME"
+rm -r /usr/local/bin/gosu.asc
 
 chmod +x /usr/local/bin/gosu
 
